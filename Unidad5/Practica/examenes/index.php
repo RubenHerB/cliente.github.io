09a7e4d2145ed853eff2c1647793157504720b55
RubenHerB <rubenhb19@educastur.es>
2024-02-15 19:14:09 +0000

    Feb 15, 2024, 8:14 PM

--- Unidad5/Practica/examenes/index.php
+++ Unidad5/Practica/examenes/index.php
@@ -6,6 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>Generador de formularios</h1>
+    <div id="control" class="control"><button onclick="crear()">Crear Pregunta</button><button onclick="generar()">Generar formulario</button></div>
+    <div id="content" class="content"></div>
 </body>
 </html>
